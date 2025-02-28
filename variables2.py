60a1071e98bad8cd6a09e1ac28be0c995591a834
techridetraining <techridetraining@gmail.com>
2025-02-28 15:41:50 +0530

    more steps in variables

---
+++ variables2.py
@@ -0,0 +1,30 @@
+#to find the variable address
+num=5
+print (id(num))
+# to find the boolean
+p=7
+q=10
+print (q>p)
+'''
+ to find a mathematical opreation like (+,-,*,/ )for any number use the logic 
+ a=int(input("Enter a number")) 
+ b=int(input("Enter a number")) 
+ optional print(int(input("Enter a number")) )
+print(a+b)
+print(a*b)
+print(a-b)
+print(a/b)
+'''
+# identify range 
+range(20)
+print(list(range(10)))
+print(type(range(10)))
+#for dictionary or map
+keys=('sneha','yash','sudha')
+values=('samsung','iphone','nokia')
+print(tuple(keys))
+data=dict(zip(keys,values))
+print (data)
+# print(dict('sneha'))
+print(data['yash'])
+print(data['sneha'])
