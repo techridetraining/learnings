a3d72115d661d310c4ad6eb19a55b5372aeaa342
techridetraining <techridetraining@gmail.com>
2025-03-04 10:24:18 +0530

    logic for arthematic operator

---
+++ operators.py
@@ -0,0 +1,17 @@
+# # logic for arthematic operator
+# a=int(input("Enter a number")) 
+# b=int(input("Enter a number")) 
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# logic for assignment operator
+a=45
+a=a+2
+a-=10
+a=a-10
+print(a)
+a=65
+b=76     
+print(a+b)                                                                                                                                                              
