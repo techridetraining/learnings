20bff8642279775d828e658bfe25f819dec7474d
techridetraining <techridetraining@gmail.com>
2025-03-04 10:55:53 +0530

    Binaryformats

---
+++ binaryformat.py
@@ -0,0 +1,19 @@
+# for binary code use the logic
+v=bin(25)
+print(v)
+# for octal use the key as
+x=oct(99)
+print(x)
+# for hexadecimal
+y=hex(7)
+print(y)
+# for bitwise operator (find binary code for -13 (denoted with tilde ~))
+# for 12 1st c.no 00001100 2nd c.no 11110011 &for +13 00001101 1st c.no 11110010 2nd c.no (add +1)11110011
+p=~12
+print(p)
+q=12&13
+print(q)
+r=45|37
+print(r)
+s=21^34
+print(s)
