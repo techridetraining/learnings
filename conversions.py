b04c9a63a07c26a08cc874bd516c6ea287ecd038
techridetraining <techridetraining@gmail.com>
2025-03-04 15:45:38 +0530

    conversions in python

---
+++ conversions.py
@@ -0,0 +1,17 @@
+# convert str to int 
+a = input("Enter a number: ")
+b = int(a)
+c = b+10
+print(c)
+
+# adding str to str
+a = input("Enter something: ")
+b = input("Enter something: ")
+c = a+" "+b
+print(c)
+
+# concatenate str to int or int to str
+a = input("Enter a something: ")
+b = input("enter a number")
+c = a+" "+str(b)
+print(c)
