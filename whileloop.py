0dbad9246dafd7c6860adab43f3da836084850c8
techridetraining <techridetraining@gmail.com>
2025-03-06 10:59:22 +0530

    while loop in python

---
+++ whileloop.py
@@ -0,0 +1,36 @@
+i=1
+h=1
+while i<=4:
+    print("variable",end="")
+    while h<=3:
+        print("values",end="")
+        h=h+1
+i=i+1
+
+i=1
+h=1
+while i<=4:
+    print("variable")
+    while h<=3:
+        print("values")
+        h=h+1
+    i=i+1
+
+s='we are good in python'
+print(s.index('o'))
+
+while loop
+n=1
+while n<=10:
+    print(n)
+    n+=1
+else:
+     print('out of loop')  
+
+n=10
+while n<=25:
+    print(n)
+    n+=3
+else:
+    print('out of loop')
+
