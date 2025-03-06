df2ca4606019128af300bbce8790f1567dd7389c
techridetraining <techridetraining@gmail.com>
2025-03-06 14:20:42 +0530

    for loop in python

---
+++ for_loop.py
@@ -0,0 +1,46 @@
+# t=1
+# for t in range(1,11):
+#     print(t)
+#     t=t+1
+
+# t=1
+# for t in range(1,11):
+#     if t%2==0:
+#         print(t)
+#     t=t+1
+
+# t=10
+# for t in range(10,0,-1):
+#     print(t)
+    
+# for t in range(5,15,+1):
+#     print(t)
+    
+# for t in range(20,10,-1):
+#     print(t)
+    
+# for t in range(1,20,3):
+#     if t==3:
+#         continue
+#     print(t)
+    
+
+# for t in range(20,1,-1):
+#     if t%2==0:
+#         print(t)
+    
+# r=int(input("enter a number"))
+# for t in range(1,11):
+#     print(f"{r}x{t}={r*t}")
+
+# for t in range(1, 6):  # Outer Loop
+#     for j in range(1, t + 1):  # Inner Loop
+#         print(j, end="")
+#     print()
+
+for t in range(1,6,):  # Outer Loop
+    for j in range(1,t+1):  # Inner Loop
+        print(t, end=" ")
+    print()
+
+
