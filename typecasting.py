6ff6202d7e9c9bf41cad4afdc12113fb54ddd124
techridetraining <techridetraining@gmail.com>
2025-03-12 15:21:25 +0530

    identify the type function

--- typecasting.py
+++ typecasting.py
@@ -1,21 +1,19 @@
-''
-a=10
-b=30
-c="puppy"
-_9g=20.45
 
-integer_9g=int(_9g)
+# a=10
+# b=30
+# c="puppy"
+# _9g=20.45
 
-print(integer_9g)
+# integer_9g=int(_9g)
 
-print (type(a))
-print (type(_9g),type(b))
-print (c)
-print (type(c))
-'''
-print int(_9g)
-print (d=int(c))
+# print(integer_9g)
 
-'''
+# print (type(a))
+# print (type(_9g),type(b))
+# print (c)
+# print (type(c))
+
 # print int(_9g)
 # print (d=int(c))
+
+
