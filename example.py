02c8de8f54ee42186884f2c33b4a71cdc8df46c6
techridetraining <techridetraining@gmail.com>
2025-03-17 16:00:05 +0530

    arrays example

---
+++ example.py
@@ -0,0 +1,29 @@
+ x = a('u',['s','u','d','h','a'])
+#  newarr =b(names.typecode,(a for a in names))
+
+#  for u in newarr:
+#      print (u)
+
+# from array import array
+
+# names = array('u', ['s', 'u', 'd', 'h', 'a'])
+
+# Creating a new array using list comprehension
+# newarr = array(names.typecode, (a for a in names))
+
+# # Printing elements
+# for u in newarr:
+#     print(u)
+
+
+# from array import array
+
+# names = array('u', ['s', 'u', 'd', 'h', 'a'])
+
+# # Creating a new array using list comprehension
+# newarr = array(names.typecode, (a for a in names))
+# print(newarr)
+
+#  Printing elements
+# for u in newarr:
+# print(u)
