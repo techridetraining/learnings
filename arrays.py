6d78a3d76088ea9e32fadccbb517c8d18dde2ac7
techridetraining <techridetraining@gmail.com>
2025-03-17 15:58:56 +0530

    arrays in datastructure

---
+++ arrays.py
@@ -0,0 +1,29 @@
+# import array as arr
+# defining the array
+# a=arr.array('d',[1.1,1.2,1.3])
+
+# adding the element
+# a.append(3.4)
+# print(a)
+
+# for extending
+# a.extend([5.6,8.6])
+# print(a)
+
+# for removing
+# a.pop()
+# print(a) or
+# print(a.pop())
+
+
+
+
+
+
+
+
+
+
+
+
+    
