aba0cd85646418455e62608e6e6c990555e7fc08
techridetraining <techridetraining@gmail.com>
2025-03-25 11:38:13 +0530

    arrays

--- arrays.py
+++ arrays.py
@@ -1,19 +1,19 @@
-# import array as arr
+import array as arr
 # defining the array
-# a=arr.array('d',[1.1,1.2,1.3])
+a=arr.array('d',[1.1,1.2,1.3])
 
-# adding the element
-# a.append(3.4)
-# print(a)
+#adding the element
+a.append(3.4)
+print(a)
 
-# for extending
-# a.extend([5.6,8.6])
-# print(a)
+#for extending
+a.extend([5.6,8.6])
+print(a)
 
-# for removing
-# a.pop()
-# print(a) or
-# print(a.pop())
+#for removing
+a.pop()
+print(a) 
+print(a.pop())
 
 
 
